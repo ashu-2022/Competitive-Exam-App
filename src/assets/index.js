648709e99da93473ef14b09bd9cd79184811633d
ashutosh-pandit <ashutosh@dev.causalfunnel.com>
2023-10-13 16:45:31 +0530

    causalfunnel logo updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,8 @@
 import JoshTalkLogo from './logos/josh-logo.svg';
 import userIcon from './icons/user.png';
+import CausalFunnelLogo from './logos/causalfunnel.svg';
 export {
     JoshTalkLogo,
-    userIcon
+    userIcon,
+    CausalFunnelLogo
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { JoshTalkLogo } from '../../assets'
+import { CausalFunnelLogo } from '../../assets'
 import './Header.scss';
 const Header = () => {
   return (
       <div className='header__container'>
-          <img className='header__logo' src={JoshTalkLogo} alt='Josh Talk Logo' />
-          <p className='header__title'>Josh Talk Frontend Engineer Assignment</p>
+          <img className='header__logo' src={CausalFunnelLogo} alt='CausalFunnel Logo' />
+          <p className='header__title'>CausalFunnel Frontend Engineer Assignment</p>
     </div>
   )
 }
