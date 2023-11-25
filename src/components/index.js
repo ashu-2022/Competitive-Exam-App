7f9dbb76b8d94df79715b92c746903c128f8ac2a
ashutosh-pandit <ashutosh@dev.causalfunnel.com>
2023-11-25 11:59:53 +0530

    clean the folder content

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-import Question from "./Question/Question";
-import ProfileMenu from "./ProfileMenu/ProfileMenu";
-import Header from "./Header/Header";
-
-export {
-    Question,
-    ProfileMenu,
-    Header
-};
